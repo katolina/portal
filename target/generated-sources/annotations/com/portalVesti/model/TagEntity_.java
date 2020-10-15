e5dfcc8f105f274e748d225803f00e42c0416283
Kata <Kata@DESKTOP-JDQLT0F>
2020-10-15 13:22:50 +0200

    commit
    Commit

--- target/generated-sources/annotations/com/portalVesti/model/TagEntity_.java
+++ target/generated-sources/annotations/com/portalVesti/model/TagEntity_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-10-15T10:41:34")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-10-15T13:19:39")
 @StaticMetamodel(TagEntity.class)
 public class TagEntity_ { 
 
--- target/generated-sources/annotations/com/portalVesti/model/VestEntity_.java
+++ target/generated-sources/annotations/com/portalVesti/model/VestEntity_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-10-15T10:41:34")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-10-15T13:19:39")
 @StaticMetamodel(VestEntity.class)
 public class VestEntity_ { 
 
